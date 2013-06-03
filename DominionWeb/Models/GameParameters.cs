43caeaaeda04285bae9198415b71cf167034044f
JaySmith <jay@jaysmith.us>
2013-06-03 15:25:11 -0500

    Added Game Parameters for Cornucopia, Hinterlands, and Dark Ages

--- DominionWeb/Models/GameParameters.cs
+++ DominionWeb/Models/GameParameters.cs
@@ -5,12 +5,16 @@ namespace DominionWeb.Models
 
     public class GameParameters 
     {
-        public bool Base { get; set; }
         public bool Alchemy { get; set; }
+        public bool Base { get; set; }
+        public bool Cornucopia { get; set; }
+        public bool DarkAges { get; set; }
+        public bool Hinterlands { get; set; }
         public bool Intrigue { get; set; }
         public bool Prosperity { get; set; }
         public bool Seaside { get; set; }
-        public bool Promo { get; set; }
+
+        public bool Promo { get; set; }        
 
         [DisplayName("Require 2 to 5 cost cards")]
         public bool RequireTwoToFiveCostCards { get; set; }
